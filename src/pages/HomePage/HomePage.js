92213f91a309e83f7797bcc3a7707d48c7810b64
Victor Arabi <victorarabi@hotmail.com>
2022-11-23 19:34:04 -0500

    created get request for profile data

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 import LoginCard from '../../components/LoginCard/LoginCard';
 import './HomePage.scss';
+
+//fetches server_URL from environment Variable
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export default function HomePage({
   isLoggedIn,
@@ -8,6 +12,23 @@ export default function HomePage({
   profileData,
   setProfileData,
 }) {
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/profile`, { withCredentials: true })
+      .then((res) => {
+        setIsLoggedIn(true);
+      })
+      .catch((err) => {
+        // If we are getting back 401 (Unauthorized) back from the server, means we need to log in
+        if (err.response.status === 401) {
+          // Update the state: done authenticating, user is not logged in
+          setIsLoggedIn(false);
+        } else {
+          console.log('Error authenticating', err);
+        }
+      });
+  });
+
   if (!isLoggedIn) {
     return <LoginCard />;
   }
